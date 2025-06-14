7e1f6e873eb2c77be4d4c2c80db51665aee02977
Jim56450 <122618399+Jim56450@users.noreply.github.com>
2025-06-14 18:49:39 +0200

    Added TicTic Filter

---
+++ src/Controller/Admin/ActivityEntityTicTicCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\ActivityEntity;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ActivityEntityTicTicCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return ActivityEntity::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('computerId'),
+            TextField::new('appName'),
+            TextField::new('windowTitle'),
+            TextField::new('url'),
+            DateTimeField::new('startTime'),
+            DateTimeField::new('endTime'),
+            //TextEditorField::new('description'),
+        ];
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        $queryBuilder->andWhere('entity.computerId = :computerId')
+            ->setParameter('computerId', 'TicTic');
+
+
+        return $queryBuilder;
+    }
+}
--- src/Controller/Admin/MacWatchDashboardController.php
+++ src/Controller/Admin/MacWatchDashboardController.php
@@ -22,28 +22,6 @@ class MacWatchDashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return $this->redirect($this->adminUrlGenerator->setController(ActivityEntityCrudController::class)->generateUrl());
-
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // 1.1) If you have enabled the "pretty URLs" feature:
-        // return $this->redirectToRoute('admin_user_index');
-        //
-        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -57,6 +35,9 @@ class MacWatchDashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::section('Computer Activity');
         yield MenuItem::linkToCrud('Activity', 'fa fa-tags', ActivityEntity::class);
+        yield MenuItem::section('Computer Activity Tic');
+        yield MenuItem::linkToCrud('Activity', 'fa fa-tags', ActivityEntity::class)
+                    ->setController(ActivityEntityTicTicCrudController::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
